c7eaa87c72e07260c5909ed4a2205530c7fbe4e5
Ikumi Sato <ikumin.gng@gmail.com>
2024-11-27 15:35:22 +0900

    Initial Commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by 佐藤郁巳 on 2024/11/27.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
